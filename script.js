aaa751e0fa9859bdc6ac2774e32af43089143d40
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-19 14:56:16 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,69 +1,59 @@
-let firstnum;
-let secondnum;
-let operation; 
+let firstnum = "";
+let secondnum = "";
+let operation = ""; 
 
-let solutionscreen = document.querySelector(".solution");
-let answerscreen = document.querySelector(".answer");
+let solutionScreen = document.querySelector(".solution");
+let answerScreen = document.querySelector(".answer");
 
-const onebtn = document.querySelector(".one").addEventListener("click", () => {getnumber(1);});
-const twobtn = document.querySelector(".two").addEventListener("click", () => {getnumber(2);});
-const threebtn = document.querySelector(".three").addEventListener("click", () => {getnumber(3);});
-const fourbtn = document.querySelector(".four").addEventListener("click", () => {getnumber(4);});
-const fivebtn = document.querySelector(".five").addEventListener("click", () => {getnumber(5);});
-const sixbtn = document.querySelector(".six").addEventListener("click", () => {getnumber(6);});
-const sevenbtn = document.querySelector(".seven").addEventListener("click", () => {getnumber(7);});
-const eigthbtn = document.querySelector(".eight").addEventListener("click", () => {getnumber(8);});
-const ninebtn = document.querySelector(".nine").addEventListener("click", () => {getnumber(9);});
-const zerobtn = document.querySelector(".zero").addEventListener("click", () => {getnumber(0);});
-const pointbtn = document.querySelector(".point").addEventListener("click", () => {getnumber(".");});
+const numberBtns = document.querySelectorAll('[data-number]');
 
 function add() {
     operation = "addition";
-    solutionscreen.textContent = "+";
+    solutionScreen.textContent = "+";
 }
 
 function subtract() {
     operation = "subtraction";
-    solutionscreen.textContent = "-";
+    solutionScreen.textContent = "-";
 }
 
 function multiply() {
     operation = "multiplication";
-    solutionscreen.textContent = "x";
+    solutionScreen.textContent = "x";
 }
 
 function divide() {
     operation = "division";
-    solutionscreen.textContent = "÷";
+    solutionScreen.textContent = "÷";
     
 }
 
 function percentage() {
     operate("percentage");
-    solutionscreen.textContent = "%";
+    solutionScreen.textContent = "%";
 }
 
 function operate() {
     if (operation === "addition") {
-        answerscreen.textContent = firstnum + secondnum;
+        answerScreen.textContent = firstnum + secondnum;
         
     }
     else if (operation === "subtraction") {
-        answerscreen.textContent = firstnum - secondnum;
+        answerScreen.textContent = firstnum - secondnum;
     }
     else if (operation === "multiplication") {
-        answerscreen.textContent = firstnum * secondnum;
+        answerScreen.textContent = firstnum * secondnum;
     }
     else if (operation === "division") {
         if (firstnum === 0 || secondnum === 0) {
-            answerscreen.textContent = "Cannot be divide by 0";
+            answerScreen.textContent = "Cannot be divide by 0";
         }
         else {
-            answerscreen = firstnum / secondnum;
+            answerScreen = firstnum / secondnum;
         }
     }
     else if (operation === "percentage") {
-        answerscreen.textContent = firstnum / 100;
+        answerScreen.textContent = firstnum / 100;
     }
 }
 
