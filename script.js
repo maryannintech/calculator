f333fd8de19fdc133a077c834e4234222f6ceeff
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-19 17:57:32 +0800

    update script.js

--- script.js
+++ script.js
@@ -4,6 +4,52 @@ let operation = "";
 
 let solutionScreen = document.querySelector(".solution");
 let answerScreen = document.querySelector(".answer");
+const onebtn = document.querySelector(".one").addEventListener("click", () => {displayNum(1);});
+const twobtn = document.querySelector(".two").addEventListener("click", () => {displayNum(2);});
+const threebtn = document.querySelector(".three").addEventListener("click", () => {displayNum(3);});
+const fourbtn = document.querySelector(".four").addEventListener("click", () => {displayNum(4);});
+const fivebtn = document.querySelector(".five").addEventListener("click", () => {displayNum(5);});
+const sixbtn = document.querySelector(".six").addEventListener("click", () => {displayNum(6);});
+const sevenbtn = document.querySelector(".seven").addEventListener("click", () => {displayNum(7);});
+const eigthbtn = document.querySelector(".eight").addEventListener("click", () => {displayNum(8);});
+const ninebtn = document.querySelector(".nine").addEventListener("click", () => {displayNum(9);});
+const zerobtn = document.querySelector(".zero").addEventListener("click", () => {displayNum(0);});
+const pointbtn = document.querySelector(".point").addEventListener("click", () => {displayNum(".");});
+const posneg = document.querySelector(".posneg").addEventListener("click", () => {})
+
+function displayNum(number) {
+    switch(number){
+        case 1:
+            displayTextContent("1");
+            break;
+        case 2:
+            displayTextContent("2");
+            break;
+        case 3:
+            displayTextContent("3");
+            break;
+        case 4:
+            displayTextContent("4");
+            break;
+        case 5:
+            displayTextContent("5");
+            break;
+        case 6:
+            displayTextContent("6");
+            break;
+        case 7:
+            displayTextContent("7");
+            break;
+        case 8:
+            displayTextContent("8");
+            break;
+        case 9:
+            displayTextContent("9");
+            break;
+        case 0:
+            displayTextContent("0");
+    }
+}
 
 function add() {
     operation = "addition";
@@ -62,6 +108,10 @@ function operate() {
 function clear() {
     window.location.reload();
 };
+
+function displayTextContent(content) {
+    solutionScreen.textContent += content;
+}
 
 const percentbtn = document.querySelector(".percentage").addEventListener("click", percentage);
 const dividebtn = document.querySelector(".divide").addEventListener("click", divide);
