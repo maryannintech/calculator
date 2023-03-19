5dff890047d96803067ce6bdf959d0e340af1307
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-19 13:07:55 +0800

    update script.js

--- script.js
+++ script.js
@@ -3,44 +3,44 @@ let currentnum;
 let solutionscreen = document.querySelector(".solution");
 let answerscreen = document.querySelector(".answer");
 
-const onebtn = document.querySelector(".one").addEventListener("click", () => {getnumber(1)});
-const twobtn = document.querySelector(".two").addEventListener("click", () => {getnumber(2)});
-const threebtn = document.querySelector(".three").addEventListener("click", () => {getnumber(3)});
-const fourbtn = document.querySelector(".four").addEventListener("click", () => {getnumber(4)});
-const fivebtn = document.querySelector(".five").addEventListener("click", () => {getnumber(5)});
-const sixbtn = document.querySelector(".six").addEventListener("click", () => {getnumber(6)});
-const sevenbtn = document.querySelector(".seven").addEventListener("click", () => {getnumber(7)});
-const eigthbtn = document.querySelector(".eight").addEventListener("click", () => {getnumber(8)});
-const ninebtn = document.querySelector(".nine").addEventListener("click", () => {getnumber(9)});
-const zerobtn = document.querySelector(".zero").addEventListener("click", () => {getnumber(0)});
-const doublezerobtn = document.querySelector(".doublezero").addEventListener("click", () => {getnumber(00)});
+const onebtn = document.querySelector(".one").addEventListener("click", () => {getnumber(1);});
+const twobtn = document.querySelector(".two").addEventListener("click", () => {getnumber(2);});
+const threebtn = document.querySelector(".three").addEventListener("click", () => {getnumber(3);});
+const fourbtn = document.querySelector(".four").addEventListener("click", () => {getnumber(4);});
+const fivebtn = document.querySelector(".five").addEventListener("click", () => {getnumber(5);});
+const sixbtn = document.querySelector(".six").addEventListener("click", () => {getnumber(6);});
+const sevenbtn = document.querySelector(".seven").addEventListener("click", () => {getnumber(7);});
+const eigthbtn = document.querySelector(".eight").addEventListener("click", () => {getnumber(8);});
+const ninebtn = document.querySelector(".nine").addEventListener("click", () => {getnumber(9);});
+const zerobtn = document.querySelector(".zero").addEventListener("click", () => {getnumber(0);});
+const doublezerobtn = document.querySelector(".doublezero").addEventListener("click", () => {getnumber(00);});
 
 function getnumber(number) {
-
+    solutionscreen.textContent = number;
 }
 
 function add(firstnum, currentnum) {
-    solutionscreen.innerText = "+";
+    solutionscreen.textContent = "+";
     let sum = firstnum + currentnum;
     return sum;
 }
 
 function subtract(firstnum, currentnum) {
-    solutionscreen.innerText = "-";
+    solutionscreen.textContent = "-";
     let difference = firstnum - currentnum;
     return difference;
 }
 
 function multiply(firstnum, currentnum) {
-    solutionscreen.innerText = "x";
+    solutionscreen.textContent = "x";
     let product = firstnum * currentnum;
     return product;
 }
 
 function divide(firstnum, currentnum) {
-    solutionscreen.innerText = "÷";
+    solutionscreen.textContent = "÷";
     if (firstnum === 0 || currentnum === 0) {
-        answerscreen.innerText = "Cannot be divide by 0";
+        answerscreen.textContent = "Cannot be divide by 0";
     }
     else {
         let quotient = firstnum / currentnum;
@@ -49,7 +49,7 @@ function divide(firstnum, currentnum) {
 }
 
 function percentage(firstnum) {
-    solutionscreen.innerText = "%";
+    solutionscreen.textContent = "%";
     let result = firstnum / 100;
     return result;
 }
