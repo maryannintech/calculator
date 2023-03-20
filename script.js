e9e3bbdff93f2eed10511e2936b60fed9e331066
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-20 18:28:41 +0800

    update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let num2;
 let operation;
 let clicked = false;
 
+
+// OPERATORS FUNCTIONS
+
 function add(x, y) {
     operation = "+";
     let result = Number(x) + Number(y);
@@ -24,7 +27,7 @@ function multiply(x, y) {
 function divide(x, y) {
     operation = "/";
     if (x === 0 || y === 0) {
-        prompt("Cannot be divide by zero");
+        alert("Cannot be divide by zero");
     }
     else {
         let result = Math.floor(Number(x) / Number(y));
@@ -32,14 +35,28 @@ function divide(x, y) {
     }
 }
 
+// OPERATORS
 const addBtn = document.querySelector(".add").addEventListener("click", add);
 const subtractBtn = document.querySelector(".subtract").addEventListener("click", subtract);
 const multiplyBtn = document.querySelector(".multiply").addEventListener("click", multiply);
-const divideBtn = document.querySelectorAll(".divide").addEventListener("click", divide);
+const divideBtn = document.querySelector(".divide").addEventListener("click", divide);
 
+// NUMBERS
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+
+// SCREEN
 const solutionScreen = document.querySelector(".solution");
-const answerScreen = document.querySelectorAll(".answer");
+const answerScreen = document.querySelector(".answer");
 
+// OTHER TOOLS
 const clearBtn = document.querySelector(".clear").addEventListener("click", clear);
 const deleteBtn = document.querySelector(".delete").addEventListener("click", backspaceClick);
 
@@ -53,7 +70,7 @@ function backspaceClick() {
         solutionScreen.textContent = solutionScreen.textContent.slice(0, -1);
         solutionScreen.textContent = "";
     }
-    // if deleting only more number
+    // if deleting more numberS
     else if (solutionScreen.textContent.length > 1) {
         solutionScreen.textContent = solutionScreen.textContent.slice(0, -1);
     }
@@ -61,6 +78,7 @@ function backspaceClick() {
         return;
     }
 }
+
 
 
 
