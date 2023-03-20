a559c1fe1783760683fd3e48e03ca0180bf56fdb
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-20 19:33:20 +0800

    remove the percentage button

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let num1;
+let num2;
 let operation;
+let result;
 let clicked = false;
 
+// SCREEN
+const solutionScreen = document.querySelector(".solution");
+const answerScreen = document.querySelector(".answer");
 
 function buttonClick(number){
+    num1 = number;
+    num2 = 2;
+     // to enable the operations and decimal point button when a number is clicked
+     opeBtns.forEach((btn) => {
+        btn.disabled = false;
+    })
     solutionScreen.textContent += number;
     num1 += number;
     return number;
@@ -13,26 +24,27 @@ function buttonClick(number){
 
 function add(x, y) {
     operation = "+";
-    let result = Number(x) + Number(y);
+    result = Number(x) + Number(y);
     return result;
 }
 
 function subtract(x, y) {
     operation = "-";
-    let result = Number(x) - Number(y);
-    return result;
+    result = Number(x) - Number(y);
+    return result
 }
 
 function multiply(x, y) {
     operation = "*";
-    let result = Number(x) * Number(y);
-    return result;
+    result = Number(x) * Number(y);
+    return result
 }
 
 function divide(x, y) {
     operation = "/";
     if (x === 0 || y === 0) {
-        alert("Cannot be divide by zero");
+        let errorsign = "Cannot be divide by 0."
+        return errorsign;
     }
     else {
         let result = Math.floor(Number(x) / Number(y));
@@ -41,31 +53,48 @@ function divide(x, y) {
 }
 
 function operate() {
-    let num2 = buttonClick;
     switch (operation) {
         case "+":
         add(num1, num2);
+        populateDisplay(result);
         break;
         case "-":
         subtract(num1, num2);
+        populateDisplay(result);
         break;
         case "*":
         multiply(num1, num2);
+        populateDisplay(result);
         break;
         case "/":
         divide(num1, num2);
+        populateDisplay(result);
         break;
     }
 }
 
-// OPERATORS
+function populateDisplay(content) {
+    answerScreen.textContent = content;
+}
+
+function displayOperator(op) {
+    solutionScreen.textContent += op;
+}
+
+// OPERATORS 
+
+let opeBtns = document.querySelectorAll(".ope");
 let addBtn = document.querySelector(".add").addEventListener("click", add);
+addBtn = document.querySelector(".add").addEventListener("click", () => {displayOperator("+");});
 addBtn = document.querySelector(".add").addEventListener("click", opClicked);
 let subtractBtn = document.querySelector(".subtract").addEventListener("click", subtract);
+subtractBtn = document.querySelector(".subtract").addEventListener("click", () => {displayOperator("+");});
 subtractBtn = document.querySelector(".subtract").addEventListener("click", opClicked);
 let multiplyBtn = document.querySelector(".multiply").addEventListener("click", multiply);
+multiplyBtn = document.querySelector(".multiply").addEventListener("click", () => {displayOperator("+");});
 multiplyBtn = document.querySelector(".multiply").addEventListener("click", opClicked);
 let divideBtn = document.querySelector(".divide").addEventListener("click", divide);
+divideBtn = document.querySelector(".divide").addEventListener("click", () => {displayOperator("+");});
 divideBtn = document.querySelector(".divide").addEventListener("click", opClicked);
 
 function opClicked() {
@@ -90,10 +119,6 @@ const sevenBtn = document.querySelector(".seven").addEventListener("click", () =
 const eightBtn = document.querySelector(".eight").addEventListener("click", () => {buttonClick(8)});
 const nineBtn = document.querySelector(".nine").addEventListener("click", () => {buttonClick(9)});
 const zeroBtn = document.querySelector(".zero").addEventListener("click", () => {buttonClick(0)});
-
-// SCREEN
-const solutionScreen = document.querySelector(".solution");
-const answerScreen = document.querySelector(".answer");
 
 // OTHER TOOLS
 const clearBtn = document.querySelector(".clear").addEventListener("click", clear);
